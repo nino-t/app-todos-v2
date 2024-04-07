fe3e45d3f13c88eeacd869b0ebade5d32475fd23
Arga <argaprataman052@gmail.com>
2024-04-07 16:45:25 +0700

    feat: Todo message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Todoitems from "./components/Todoitems";
+
 function App() {
   return (
     <div className="app-container">
-      <span>Hello World</span>
+      <Todoitems />
+      <Todoitems />
+      <Todoitems />
+      <Todoitems />
+      <Todoitems />
+      <button className="btn-add">New</button>
     </div>
   );
 }
